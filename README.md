6e9ab9d00f56357375a127939fc2ace526043e79
Gil Pratte <gil.pratte@mutualmobile.com>
2018-06-07 11:28:15 -0500

    http get one hero

--- README.md
+++ README.md
@@ -24,3 +24,4 @@ Here are the branches:
 * step-17-detail-go-back
 * step-18-http-client (get many works but not get one)
 * step-19-error-handling (get many works but not get one)
+* step-20-http-get-one
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -26,11 +26,13 @@ export class HeroService {
       );
   }
 
+  /** GET hero by id. Will 404 if id not found */
   getHero(id: number): Observable<Hero> {
-    // TODO: send the message _after_ fetching the hero
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    //return of(HEROES.find(hero => hero.id === id));
-    return null;
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
   }
 
   /** Log a HeroService message with the MessageService */
